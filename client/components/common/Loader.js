4ea36aad9bb86d0f27f118245235c09af63a024e
Tim Kozak <>
2020-03-13 00:19:56 +0200

    loader upgrade

--- client/components/common/Loader.js
+++ client/components/common/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import loader from './img/loader.png'
+import loader from './img/loader.gif'
+import s from './Loader.module.scss'
 
 export const Loader = (props) => {
-    return <img src={loader} alt=""/>;
+    return <div className={s.loader}><img src={loader} alt=""/></div>;
 };
