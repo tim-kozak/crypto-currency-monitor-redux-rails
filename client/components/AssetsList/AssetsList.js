47aa667dec9cba9655a089771a0e97ea8f7c2c19
Tim Kozak <>
2020-03-14 00:52:57 +0200

    asset updates WIP

--- client/components/AssetsList/AssetsList.js
+++ client/components/AssetsList/AssetsList.js
@@ -6,12 +6,19 @@ import {COLORS} from "../../utils/colors";
 export const AssetsList = (props) => {
     const {assets, currencies, handleAddAsset} = props;
     const [state, setState] = useState({
-        isEditing: false
+        isEditing: false,
+        isAdding: false
     });
 
     const handleEdit = () => {
         setState({
             isEditing: !state.isEditing
+        })
+    };
+
+    const handleAdd = () => {
+        setState({
+            isAdding: !state.isAdding
         })
     };
 
@@ -24,15 +31,31 @@ export const AssetsList = (props) => {
                     const amount = simpleValue(asset ? asset.amount : 0, 3);
                     const value = simplePrice(currency.price * amount,1);
                     return (<li>
-                                <div>
-                                    <span style={{ backgroundColor: COLORS[index]}}>{assetName}:</span>
+                                <div className={s.first}>
+                                    <span className={s.number}>{index+1}.</span>
+                                    <span className={s.currency} style={{ backgroundColor: COLORS[index]}}>{assetName}:</span>
                                 </div>
-                                {state.isEditing ? (<input type="text" value={amount}/>) : (<span className={s.amount} onClick={handleEdit}>{amount}</span>)}
-                                <strong className={s.value}>{value}</strong>
+                                <div className={s.second}>
+                                    {state.isEditing ? (<input type="text" value={amount}/>) : (<span className={s.amount} onClick={handleEdit}>{amount}</span>)}
+                                </div>
+                                <div className={s.third}>
+                                    <span className={s.value}>{value}</span>
+                                </div>
                             </li>);
                 })}
             </ul>
-            <a className={s.add} onClick={()=> { handleAddAsset() }}>+ Add Asset</a>
+            { state.isAdding ? (
+                <form>
+                    <select name="" id="">
+                        <option value='1' />
+                    </select>
+                    <input />
+                    <button type="cancel">Cancel</button>
+                    <button type="submit">Save</button>
+                </form>
+            ) : (
+                <a className={s.add} onClick={handleAdd}>+ Add Asset</a>
+            )}
         </div>
     );
 };
